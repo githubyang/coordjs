7f6bc3780cb98642d01afa3e91c80661d56794f0
yang <281627989@qq.com>
2015-03-18 16:28:59 +0800

    add

--- README.md
+++ README.md
@@ -1,2 +1,39 @@
 # coordjs
 经纬度坐标获取
+
+获取用户的地理位置,使用它需要得到用户的授权,浏览器会跳出一个对话框,询问用户是否许可当前页面获取他的地理位置.
+目前只能支持经纬度获取和监听用户位置的持续改变.
+
+# API
+
+```javascript
+// 获取用户位置信息
+P.coords(function(){
+  alert('您的经度:'+this.longitude+';您的纬度:'+this.latitude);
+  // this是返回的用户地理位置信息如下
+  {
+    is:'布尔值 代表获取成功还是失败',
+    longitude:'经度'
+    latitude:'纬度',
+    accuracy:'精度'    
+    altitude:'海拔'
+    altituedAccuracy:'海拔精度(单位:m)',
+    heading:'方向 360度表示'
+    speed:'每秒速度(单位:m)'
+    code:'错误代码',
+    message:'错误消息'
+  };
+  // 如果出错了 以下为错误代码
+	0: 未知错误,浏览器没有提示出错的原因,相当于常量event.UNKNOWN_ERROR
+  1: 用户拒绝授权,相当于常量event.PERMISSION_DENIED
+  2: 没有得到位置,GPS或其他定位机制无法定位,相当于常量event.POSITION_UNAVAILABLE
+  3: 超时,GPS没有在指定时间内返回结果,相当于常量event.TIMEOUT
+});
+
+// 监听用户位置的持续改变
+P.watchPosition(function(){
+	//返回的数据和上面的一样,只不过是它是持续监听
+});
+// 取消持续监听
+P.clearWatch();
+```
---
+++ coord.js
@@ -0,0 +1,74 @@
+﻿/*------------------------
+ * 经纬度获取
+ * 2015.3.18 yang
+ ------------------------*/
+var P=(function(){
+  var isGeolocation,
+      obj,
+      watchId,
+      callback=null,
+      option={
+        enableHighAccuracy:false,//默认值 如果设为true,就要求客户端提供更精确的位置信息,这会导致更长的定位时间和更大的耗电,默认设为false
+        timeout:10000,//默认值 等待客户端做出回应的最大毫秒数,默认值为Infinity(无限)
+        maximumAge:0//默认值 客户端可以使用缓存数据的最大毫秒数,如果设为0,客户端不读取缓存,如果设为infinity,客户端只读取缓存
+      },
+      getSuccess=function(o){
+        obj={
+          is:!0,
+          longitude:(o.coords.longitude||null),//经度
+          latitude:(o.coords.latitude||null),//纬度
+          accuracy:(o.coords.accuracy||null),//精度     
+          altitude:(o.coords.altitued||null),//海拔
+          altituedAccuracy:(o.coords.altituedAccuracy||null),//海拔精度(单位:m)
+          heading:(o.coords.heading||null),//方向 360度表示
+          speed:(o.coords.speed||null)//每秒速度(单位:m)
+        };
+        callback.call(obj);
+      },
+      handleError=function(o){
+      /*            
+        0: 未知错误,浏览器没有提示出错的原因,相当于常量event.UNKNOWN_ERROR
+        1: 用户拒绝授权,相当于常量event.PERMISSION_DENIED
+        2: 没有得到位置,GPS或其他定位机制无法定位,相当于常量event.POSITION_UNAVAILABLE
+        3: 超时,GPS没有在指定时间内返回结果,相当于常量event.TIMEOUT
+      */
+      obj={
+        is:!1,
+        code:o.code,
+        message:o.message
+      };
+      callback.call(obj);
+    };
+    navigator.geolocation&&(isGeolocation=!0);
+    return {
+      coords:function(fn){
+        callback=fn;
+        if(isGeolocation){
+          navigator.geolocation.getCurrentPosition(getSuccess,handleError,option);
+        }
+        return this;
+      },
+      watchPosition:function(fn){
+        callback=fn;
+        if(isGeolocation){
+          watchId=navigator.geolocation.watchPosition(getSuccess,handleError,option);
+        }
+        return this;
+      },
+      clearWatch:function(){
+        watchId&&navigator.geolocation.clearWatch(watchId);
+      },
+      config:function(opt){
+        if(typeof opt==='object'){
+          for(var i in option){
+            option.i=opt.i;
+          }
+        }
+        return this;
+      }
+    };
+}());
+
+P.coords(function(){
+  alert('您的经度:'+this.longitude+';您的纬度:'+this.latitude);
+});
